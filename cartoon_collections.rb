f4afc36cadce05904e11d2104df126f1fac71fb0
Luis <lvas248@gmail.com>
2022-09-23 18:27:05 -0400

    commit tree 8063b280d1e8c221385782fc54927000782c62d7

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index do |dwarf, index|
+    puts "#{index+1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |name|
+    name.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array)
+  test_array = array.filter do |word|
+    word.length > 4
+  end
+  (test_array.length > 0)
 end
 
-def find_the_cheese# code an argument here
+
+
+
+
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  #for each chees type, find the chees in the list of ingredients
+  cheese_types.find do |cheese|
+    array.find do |ingr|
+      ingr == cheese
+    end
+  end
 end
+
+summon_captain_planet(["luis","sasha"])
